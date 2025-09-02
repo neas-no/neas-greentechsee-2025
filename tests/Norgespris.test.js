7c56204e16702beb0c380eab3394d7489b53213e
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-09-02 16:02:29 +0200

    Testing PR check with working unit test

--- tests/Norgespris.test.js
+++ tests/Norgespris.test.js
@@ -2,6 +2,6 @@ import { render, screen } from "@testing-library/vue";
 import Norgespris from "../src/components/Norgespris.vue";
 
 test("renders props.msg when passed", () => {
-  render(Norgespris, { props: { msg: "Spørsmål og svar om Norgespris" } });
-  screen.getByText("Spørsmål og svar om Norgespris"); // passes if text is found
+  render(Norgespris, { props: { msg: "Kommer det tillegg utover 40 øre?" } });
+  screen.getByText("Kommer det tillegg utover 40 øre?"); // passes if text is found
 });
