6bfc245143dcab5a21df617868fe3569bd15775c
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-09-02 16:00:03 +0200

    Broke test to see if PR check works

--- tests/Norgespris.test.js
+++ tests/Norgespris.test.js
@@ -3,5 +3,5 @@ import Norgespris from "../src/components/Norgespris.vue";
 
 test("renders props.msg when passed", () => {
   render(Norgespris, { props: { msg: "Spørsmål og svar om Norgespris" } });
-  screen.getByText("Spørsmål og svar om Norgespris"); // passes if text is found
+  screen.getByText("Spørsmål og svar om Norgespris?"); // passes if text is found
 });
