82c151b2eae4066140773aed942912bc49ec7ab3
runenordskag <runen@neas.mr.no>
2025-09-02 14:33:51 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import Norgespris from './components/Norgespris.vue';
 </template>
 
 <style scoped>
+
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -20,4 +21,28 @@ import Norgespris from './components/Norgespris.vue';
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+/* Global styles can go here */
+
+/* Font Mabry */
+@font-face {
+  font-family: 'Mabry';
+  src: url('/fonts/mabry-regular-pro.woff2') format('woff2'),
+       url('/fonts/mabry-regular-pro.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Mabry-Light';
+  font-weight: 300;
+  src: url('/fonts/mabry-light-pro.woff2') format('woff2'),
+       url('/fonts/mabry-light-pro.woff') format('woff');
+}
+
+@font-face {
+  font-family: 'Mabry-Medium';
+  font-weight: 500;
+  src: url('/fonts/mabry-medium-pro.woff2') format('woff2'),
+       url('/fonts/mabry-medium-pro.woff') format('woff');
+}
+
 </style>
